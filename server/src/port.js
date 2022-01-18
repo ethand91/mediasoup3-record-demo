f34709a7ad458a1166103e63f4658ef5d6afa04b
Ethan <ethan@maru.jp>
2022-01-18 16:31:43 +0900

    del: unneeded port check

--- server/src/port.js
+++ server/src/port.js
@@ -11,14 +11,6 @@ module.exports.getPort = async () => {
 
   while(takenPortSet.has(port)) {
     port = getRandomPort();
-
-    try {
-      // Check that the port is available to use
-      await isPortOpen(port);
-    } catch (error) {
-      console.error('getPort() port is taken [port:%d]', port);
-      takenPortSet.add(port);
-    }
   }
 
   takenPortSet.add(port);
@@ -29,16 +21,3 @@ module.exports.getPort = async () => {
 module.exports.releasePort = (port) => takenPortSet.delete(port);
 
 const getRandomPort = () => Math.floor(Math.random() * (MAX_PORT - MIN_PORT + 1) + MIN_PORT); 
-
-// Users a socket to check that the port is open
-const isPortOpen = (port) => {
-  return new Promise((resolve, reject) => {
-    socket.once('connect', () => resolve); 
-    
-    socket.setTimeout(TIMEOUT);
-    socket.once('timeout', () => reject);
-    socket.once('error', (error) => reject());
-
-    socket.connect(port, '127.0.0.1');
-  });
-};
